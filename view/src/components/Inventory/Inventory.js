7a0b12c545d816b04acca7b5f5dfd7f23fc0c480
Matthew Cibulka <mcibulkadev@gmail.com>
2018-09-22 16:09:39 -0400

    Added species family accordion with species + checkbox inside.

--- view/src/components/Inventory/Inventory.js
+++ view/src/components/Inventory/Inventory.js
@@ -3,9 +3,8 @@ import React from "react";
 class Inventory extends React.Component {
   render() {
     return (
-      <div>
+      <div className="Inventory">
         <h1>Inventory</h1>
-
         <div className="container">
           <div className="row">
             <div className="col">
@@ -21,8 +20,40 @@ class Inventory extends React.Component {
             <div className="col">
             </div>
           </div>
+          <div className="row align-items-start">
+            <div className="col">
+            </div>
+            <div className="col-10">
+              <div className="accordion" id="familyAccordion">
+                <div className="card">
+                  <div className="card-header" id="speciesOne">
+                    <h5 className="mb-0">
+                      <button className="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Species Family #1
+                      </button>
+                    </h5>
+                  </div>
+                  <div id="collapseOne" className="collapse show" aria-labelledby="speciesOne" data-parent="#familyAccordion">
+                    <div className="card-body">
+                      <label for="basic-url">Species</label>
+                      <div class="input-group input-group-lg mb-3">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="speciesText1">Species #1</span>
+                        </div>
+                        <div class="input-group-text">
+                          <input type="checkbox" aria-label="Checkbox for following text input"></input>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+            </div>
+          </div>
         </div>
-      </div>        
+      </div>
     );
   }
 }
