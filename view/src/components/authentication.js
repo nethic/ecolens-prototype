46ee9cc04a708a97b4950bfd383bbf269139eda1
Matthew Cibulka <mcibulkadev@gmail.com>
2018-09-27 14:04:39 -0400

    Added and configured ESLint + code style and syntax fix for authentication.

--- view/src/components/authentication.js
+++ view/src/components/authentication.js
@@ -1,150 +1,146 @@
-
-import React, { Component } from 'react';
+import React from 'react';
 
 class Authentication extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            user: '',
-            pass: '',
-            response: '',
-            authRes: '',
-            token: '',
-            isAuth: false,
-        };
-        this.handleUser = this.handleUser.bind(this);
-        this.handlePass = this.handlePass.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleSignup = this.handleSignup.bind(this);
-        this.callApi = this.callApi.bind(this);
-        this.saveToken = this.saveToken.bind(this);
-    }
-
-    componentDidMount() {
-        this.saveToken();
-        if (localStorage.getItem('tkkn')) {
-            let data = JSON.stringify({
-                token: localStorage.getItem('tkkn')
-            });
-            this.callApi('/token', data)
-                .then(res => {
-                    this.setState({ authRes: res.message, isAuth: res.ans });
-                    this.props.checkAuth(res.ans);
-                })
-                .catch(err => console.log(err));
-        }
-    }
-
-    callApi = async (route, data) => {
-
-        switch (route) {
-            case '/auth/login':
-                var responseLog = await fetch(route, {
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
-                    },
-                    body: data
-                })
-                var bodyLog = await responseLog.json();
-
-                if (responseLog.status !== 200) throw Error(bodyLog.message);
-
-                return bodyLog;
-
-            case '/auth/signup':
-                var responseSign = await fetch(route, {
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
-                    },
-                    body: data
-                })
-                var bodySign = await responseSign.json();
-
-                if (responseSign.status !== 200) throw Error(bodySign.message);
-
-                return bodySign;
-            case '/token':
-                var tokenRes = await fetch(route, {
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
-                    },
-                    body: data
-                })
-                var tkbody = await tokenRes.json();
-
-                if (tokenRes.status !== 200) throw Error(tkbody.message);
-
-                return tkbody;
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: '',
+      pass: '',
+      response: '',
+      authRes: '',
+      token: '',
+      isAuth: false,
     };
+    this.handleUser = this.handleUser.bind(this);
+    this.handlePass = this.handlePass.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSignup = this.handleSignup.bind(this);
+    this.callApi = this.callApi.bind(this);
+    this.saveToken = this.saveToken.bind(this);
+  }
 
-    handleUser(event) {
-        this.setState({ user: event.target.value });
-    }
-
-    handlePass(event) {
-        this.setState({ pass: event.target.value });
-    }
-
-    handleSubmit(event) {
-        var data = JSON.stringify({
-            user: this.state.user,
-            pass: this.state.pass
+  componentDidMount() {
+    this.saveToken();
+    if (localStorage.getItem('tkkn')) {
+      let data = JSON.stringify({
+        token: localStorage.getItem('tkkn')
+      });
+      this.callApi('/token', data)
+        .then(res => {
+          this.setState({ authRes: res.message, isAuth: res.ans });
+          this.props.checkAuth(res.ans);
         })
-
-        alert('A name was submitted: ' + this.state.user + ' also a password.. opsie: ' + this.state.pass);
-        this.callApi('/auth/login', data).then(res => {
-            let token = res.token
-            localStorage.setItem('tkkn', token);
-            this.setState({ token: token, authRes: res.message, isAuth: res.ans });
-            this.props.checkAuth(res.ans);
-        });
-        event.preventDefault();
+        .catch(err => console.log(err));
     }
+  }
 
-    saveToken() {
-        if (localStorage.hasOwnProperty('tkkn')) {
-            this.setState({ token: localStorage.getItem('tkkn') });
-        }
+  async callApi(route, data) {
+    switch (route) {
+      case '/auth/login':
+        var responseLog = await fetch(route, {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: data
+        })
+        var bodyLog = await responseLog.json();
+
+        if (responseLog.status !== 200) throw Error(bodyLog.message);
+
+        return bodyLog;
+
+      case '/auth/signup':
+        var responseSign = await fetch(route, {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: data
+        })
+        var bodySign = await responseSign.json();
+
+        if (responseSign.status !== 200) throw Error(bodySign.message);
+
+        return bodySign;
+        
+      case '/token':
+        var tokenRes = await fetch(route, {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: data
+        })
+        var tkbody = await tokenRes.json();
+
+        if (tokenRes.status !== 200) throw Error(tkbody.message);
+
+        return tkbody;
     }
+  };
 
-    handleSignup(event) {
+  handleUser(event) {
+    this.setState({ user: event.target.value });
+  }
 
-        var data = JSON.stringify({
-            user: this.state.user,
-            pass: this.state.pass
-        });
+  handlePass(event) {
+    this.setState({ pass: event.target.value });
+  }
 
-        alert('A name was submitted: ' + this.state.user + ' also a password.. opsie: ' + this.state.pass);
-        this.callApi('/auth/signup', data).then(res => this.setState({ authRes: res.message }));
-        event.preventDefault();
+  handleSubmit(event) {
+    var data = JSON.stringify({
+      user: this.state.user,
+      pass: this.state.pass
+    })
 
+    alert('A name was submitted: ' + this.state.user + ' also a password.. opsie: ' + this.state.pass);
+    this.callApi('/auth/login', data).then(res => {
+      let token = res.token
+      localStorage.setItem('tkkn', token);
+      this.setState({ token: token, authRes: res.message, isAuth: res.ans });
+      this.props.checkAuth(res.ans);
+    });
+    event.preventDefault();
+  }
+
+  saveToken() {
+    if (localStorage.hasOwnProperty('tkkn')) {
+      this.setState({ token: localStorage.getItem('tkkn') });
     }
+  }
 
-    render() {
-        return (
-            <form>
-                <p className="App-intro">{this.state.response}</p>
-                <p className="App-intro">{this.state.authRes}</p>
-                <label>
-                    <input id="user" type='text' value={this.state.value} onChange={this.handleUser} placeholder="username" />
-                    <br />
-                    <input id="pass" type='password' value={this.state.value} onChange={this.handlePass} placeholder="password" />
-                </label>
-                <br />
-                <input type="submit" value="Submit" onClick={this.handleSubmit} />
-                <input type="submit" value="Signup" onClick={this.handleSignup} />
-            </form>
-        );
-    }
+  handleSignup(event) {
+    var data = JSON.stringify({
+      user: this.state.user,
+      pass: this.state.pass
+    });
 
+    alert('A name was submitted: ' + this.state.user + ' also a password.. opsie: ' + this.state.pass);
+    this.callApi('/auth/signup', data).then(res => this.setState({ authRes: res.message }));
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form>
+        <p className="App-intro">{this.state.response}</p>
+        <p className="App-intro">{this.state.authRes}</p>
+        <label>
+          <input id="user" type='text' value={this.state.value} onChange={this.handleUser} placeholder="username" />
+          <br />
+          <input id="pass" type='password' value={this.state.value} onChange={this.handlePass} placeholder="password" />
+        </label>
+        <br />
+        <input type="submit" value="Submit" onClick={this.handleSubmit} />
+        <input type="submit" value="Signup" onClick={this.handleSignup} />
+      </form>
+    );
+  }
 }
 
 export default Authentication;
